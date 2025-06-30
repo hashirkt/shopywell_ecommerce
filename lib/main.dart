4ee942a270445e24c5658d327f89c25c62d03376
hashirkt <hashirkt3132@gmail.com>
2025-06-30 09:44:27 +0530

    onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shopywell/presentation/auth/signin_screen/signin_screen.dart';
+import 'package:shopywell/presentation/splash_screen/splash_screen.dart';
 import 'package:shopywell/utils/theme.dart';
 
 void main() {
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           theme: AppTheme.lightTheme,
           debugShowCheckedModeBanner: false,
-          home: SigninScreen(),
+          home: SplashScreen(),
         );
       },
     );
